1432f685ace91ab17dbc7120878503af90b64c24
lyashenkoav92@gmail.com <andreiliashenko@users.noreply.github.com>
2015-03-09 21:17:34 +0000

    1) Ride entity integration test implemented 2) RideImpl toString method implemented 3) Boolean transformer fixed - correct handling of null cases 4) Transformation of null values in TransformingResultSet fixed 5) Region test fixed - stations nullifying relocated 6) Working text file with test ordering removed

--- src/main/java/com/anli/busstation/dal/sql/transformation/BooleanTransformer.java
+++ src/main/java/com/anli/busstation/dal/sql/transformation/BooleanTransformer.java
@@ -6,15 +6,12 @@ public class BooleanTransformer implements SqlTransformer<Boolean, Integer> {
 
     @Override
     public Integer toSql(Boolean source) {
-        if (source == null) {
-            return null;
-        }
-        return source ? 1 : 0;
+        return Boolean.TRUE.equals(source) ? 1 : 0;
     }
 
     @Override
     public Boolean toJava(Integer source) {
-        return source == null ? null : source.equals(1);
+        return source != null && !source.equals(0);
     }
 
     @Override
