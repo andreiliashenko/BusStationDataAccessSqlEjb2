228734b15f91792373138f8f4cac66b7a277f675
lyashenkoav92@gmail.com <lyashenkoav92@gmail.com@a48bbefb-d17f-25e1-05e3-0a94f7aebdbd>
2015-03-21 19:57:58 +0000

    GlassFish confugurator implemented, stubs in configurable points of BusStationDataAccessTestingSql and BusStationDataAccessSqlEjb2 are removed

--- BusStationDataAccessSqlEjb2/trunk/src/main/java/com/anli/busstation/dal/ejb2/factories/TransactionManagerFactory.java
+++ BusStationDataAccessSqlEjb2/trunk/src/main/java/com/anli/busstation/dal/ejb2/factories/TransactionManagerFactory.java
@@ -1,10 +1,11 @@
 package com.anli.busstation.dal.ejb2.factories;
 
+import com.anli.configuration.Configurator;
 import javax.transaction.TransactionManager;
 
 public class TransactionManagerFactory extends AbstractLookupFactory {
 
     public TransactionManager getManager() {
-        return lookup("java:appserver/TransactionManager", TransactionManager.class);
+        return lookup(Configurator.getTransationManagerJndiName(), TransactionManager.class);
     }
 }
